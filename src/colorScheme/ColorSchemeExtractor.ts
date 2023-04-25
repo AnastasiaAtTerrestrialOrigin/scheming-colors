a558ff13dd11c8670fb569327d9c19e1d7261d04
Anastasia <anastasia.rohner@terrestrialorigin.com>
2023-04-25 00:08:26 -0500

    old format

--- src/colorScheme/ColorSchemeExtractor.ts
+++ src/colorScheme/ColorSchemeExtractor.ts
@@ -1,7 +1,8 @@
 
 export class ColorSchemeExtractor {
   private static rgbRegex = /rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)|rgba\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),\s*([\d.]+)\)/gi;
-  private static hexRegex = /([#]?[A-Fa-f0-9]{6}|[#]?[A-Fa-f0-9]{3})/g;
+  private static hexRegex = /#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})/g;
+  private static funnyHexRejex = /"hex":"[A-Fa-f0-9]{6}"/g;
 
   private static rgbToHex(r: number, g: number, b: number): string {
     return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
@@ -16,7 +17,7 @@ export class ColorSchemeExtractor {
     while ((match = ColorSchemeExtractor.hexRegex.exec(content)) !== null) {
       let color = match[0].toLowerCase();
         color = color.charAt(0) === "#" ? color : "#" + color;
-        hexColors.push();
+        hexColors.push(color);
     }
 
     // Find and store rgb and rgba codes
@@ -26,6 +27,15 @@ export class ColorSchemeExtractor {
       const b = parseInt(match[3] || match[6], 10);
       const hexColor = ColorSchemeExtractor.rgbToHex(r, g, b);
       rgbColors.push(hexColor.toLowerCase());
+    }
+    
+    //old coolors format
+    while ((match = ColorSchemeExtractor.funnyHexRejex.exec(content)) !== null) {
+        let foundStr = match[0];
+        foundStr = foundStr.replace("\"", "");
+        foundStr = foundStr.replace("hex:", "");
+        foundStr = "#"+foundStr;
+        hexColors.push(foundStr);
     }
 
     // Combine the found hex and RGB colors
